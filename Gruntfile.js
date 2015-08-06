4c79bfc56b31ff7f2f365b31aec9a8cacddb3cb2
Nadan Gergeo <nadan@kth.se>
2015-08-06 17:42:40 +0200

    This is starting to look classy. Woop woop.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,46 @@ module.exports = function(grunt) {
         less: {
             development: {
                 files: {
-                    "dist/style.css": "src/less/style.less"
+                    "dist/css/style.css": "src/less/style.less"
                 }
             }
+        },
+
+        copy: {
+            main: {
+                files: [
+                    { expand: true, cwd: "src/", src: ["*.html"], dest: "dist/" },
+                    { expand: true, cwd: "src/", src: ["js/**/*.js"], dest: "dist/" },
+                    { expand: true, cwd: "assets/", src: ["**/*.*"], dest: "dist/" },
+                    { expand: true, cwd: "bower_components/", src: ["modernizr/modernizr.js"], dest: "dist/js/" }
+                ]
+            }
+        },
+
+        watch: {
+          scripts: {
+            files: ['src/**/*.less', 'src/**/*.js', 'src/**/*.html'],
+            tasks: ['build'],
+            options: {
+              spawn: false,
+            },
+          },
+        },
+
+        connect: {
+          server: {
+            options: {
+              port: 8000,
+              hostname: '*',
+              base: 'dist'
+            }
+          }
         }
     };
 
     grunt.initConfig(config);
 
-    grunt.registerTask("build", "less:development");
+    grunt.registerTask("build", ["less:development", "copy:main"]);
 
-    grunt.registerTask("default", "build");
+    grunt.registerTask("default", ["build", "connect", "watch"]);
 };
