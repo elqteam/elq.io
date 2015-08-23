3867e3a260e962bcaa66f101c7b5c662062a0739
Nadan Gergeo <nadan@kth.se>
2015-08-23 22:00:13 +0200

    Did some work on the first demo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 Modernizr.addTest("hyphens", Modernizr.testAllProps('hyphens'));
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var elq = Elq();
+  elq.use(elqBreakpoints);
+  elq.use(elqMirror);
+  elq.start(document.querySelectorAll("[elq]"));
+});
+
+// function createModule() {
+//     var div = document.createElement("div");
+//     div.className = "demo-module";
+//     div.setAttribute("elq");
+//     div.setAttribute("elq-breakpoints");
+//     div.setAttribute("elq-breakpoints-width", "100 200");
+//     return div;
+// }
